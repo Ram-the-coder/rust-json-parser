d72cd90c5ae05472c834c32a8fe6413208973955
ram-the-coder <ramvard@gmail.com>
2024-02-11 18:59:00 +0530

    init rust project

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    println!("{}", rust_json_parser::add(2, 4))
+}
+
---
+++ tests/test_parser.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn test_add() {
+    assert_eq!(rust_json_parser::add(1, 2), 3)
+  }
+}
